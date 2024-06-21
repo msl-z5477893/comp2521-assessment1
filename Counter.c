66cf247806c039eb3582d29197fb39291ba157e6
Michael Stephen Lape <z5477893@ad.unsw.edu.au>
2024-06-21 19:51:03 +1000

    initial commit made, tests are included for easier porting

---
+++ Counter.c
@@ -0,0 +1,38 @@
+// Implementation of the Counter ADT
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "Counter.h"
+
+struct counter {
+	
+};
+
+Counter CounterNew(void) {
+	return NULL;
+}
+
+void CounterFree(Counter c) {
+
+}
+
+void CounterAdd(Counter c, char *character) {
+
+}
+
+int CounterNumItems(Counter c) {
+	return 0;
+}
+
+int CounterGet(Counter c, char *character) {
+	return 0;
+}
+
+struct item *CounterItems(Counter c, int *numItems) {
+	return NULL;
+}
+
