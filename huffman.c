0487aa77173f8a357a4c8ffe210815a2f0db28d8
Michael Stephen Lape <z5477893@ad.unsw.edu.au>
2024-06-22 22:25:34 +1000

    started task 1 added .clang-format for consistent formatting

--- huffman.c
+++ huffman.c
@@ -6,23 +6,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "character.h"
 #include "Counter.h"
 #include "File.h"
+#include "character.h"
 #include "huffman.h"
+
+// CUSTOM HELPER FUNCTIONS
+bool is_leaf(struct huffmanTree *);
 
 // Task 1
 void decode(struct huffmanTree *tree, char *encoding, char *outputFilename) {
+        File file = FileOpenToWrite(outputFilename);
+        size_t encoding_ptr = 0;
+        struct huffmanTree *root = tree;
+        struct huffmanTree *tree_ptr = tree;
+        while (encoding[encoding_ptr] != '\0') {
+                if (is_leaf(tree_ptr)) {
+                        FileWrite(file, tree_ptr->character);
+                        tree_ptr = root;
+                        continue;
+                }
 
+                if (encoding[encoding_ptr] == '0') {
+                        tree_ptr = tree_ptr->left;
+                } else if (encoding[encoding_ptr] == '1') {
+                        tree_ptr = tree_ptr->right;
+                }
+        }
+        FileClose(file);
+}
+
+bool is_leaf(struct huffmanTree *tree) {
+        return tree->left == NULL && tree->right == NULL;
 }
 
 // Task 3
-struct huffmanTree *createHuffmanTree(char *inputFilename) {
-	return NULL;
-}
+struct huffmanTree *createHuffmanTree(char *inputFilename) { return NULL; }
 
 // Task 4
-char *encode(struct huffmanTree *tree, char *inputFilename) {
-	return NULL;
-}
+char *encode(struct huffmanTree *tree, char *inputFilename) { return NULL; }
 
